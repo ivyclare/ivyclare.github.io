08f44a022aaa62e3c84044cbfd74cc0eeee340f7
Ngong Ivoline Clarisse <ivolinengong@gmail.com>
2020-05-11 19:25:03 +0300

    remove text

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "PATE Framework",
-        "excerpt":"Due to the lack of regularly updated database of COVID-19 cases in Senegal, I decided to build an open database and a web application to display this information.      ","categories": ["comic"],
+        "excerpt":"     ","categories": ["comic"],
         "tags": [],
         "url": "http://localhost:4000/comic/pate/",
         "teaser": "http://localhost:4000/assets/images/pate.png"
