d83adfde5f9698165b242ac4d7c9ca4bc7154fff
Ngong Ivoline Clarisse <ivolinengong@gmail.com>
2020-05-12 13:57:05 +0300

    added excerpt for pate

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "PATE Framework",
-        "excerpt":"     ","categories": ["comic"],
+        "excerpt":"Do you want to join the hospital team in implementing the classifier for COVID19 through Differential Privacy?. Then head over to this [here]((https://zumrutmuftuoglu.github.io/OM-Study-Group/pate-on-covid/) to join them. You will find a coding tutorial that shows a step by step hands on implementation of the classifier. The Github repository can be found...","categories": ["comic"],
         "tags": [],
         "url": "http://localhost:4000/comic/pate/",
         "teaser": "http://localhost:4000/assets/images/pate.png"
